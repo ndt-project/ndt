ceaa1c36d5ed214a429594a9d746f9f32b11e2ac
jslawins <jslawins@889c6698-20a3-11df-95ad-bbeafe1f0ab2>
2006-05-30 17:10:06 +0000

    Removed the GPL license header from the previously added files.

--- branches/mdndt/src/network.c
+++ branches/mdndt/src/network.c
@@ -1,21 +1,9 @@
 /*
- * Enhancements of NDT (Middlebox detector based on NDT)
- * Copyright (C) 2006 jeremian <jeremian [at] poczta.fm>
+ * This file contains the functions needed to handle network related
+ * stuff.
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
- *
+ * Jakub S³awiñski 2006-05-30
+ * jeremian@poczta.fm
  */
 
 #include "network.h"
