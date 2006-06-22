c51513fb2dbed8118a88edae2d22747bcb6a8688
jslawins <jslawins@889c6698-20a3-11df-95ad-bbeafe1f0ab2>
2006-06-22 20:49:00 +0000

    Modified the Web100 variables descriptions.

--- branches/mdndt/src/varinfo.h
+++ branches/mdndt/src/varinfo.h
@@ -12,7 +12,7 @@
 char* web100vartable[][2] = {
   {"AckPktsIn", "???\nThe number of ACK packets received.\n"},
   {"AckPktsOut", "???\nThe number of ACK packets sent.\n"},
-  {"BytesRetrans", "???\nThe number of retransmitted bytes.\n"},
+  {"BytesRetrans", "The number of octets retransmitted.\n"},
   {"CongAvoid", "The number of times the congestion window has been\n"
                 "increased by the Congestion Avoidance algorithm.\n"},
   {"CongestionOverCount", "The number of congestion events which were 'backed out' of\n"
@@ -36,14 +36,24 @@ char* web100vartable[][2] = {
   {"CurSsthresh", "The current slow start threshold in octets.\n"},
   {"DSACKDups", "The number of duplicate segments reported to the local host\n"
                 "by D-SACK blocks.\n"},
-  {"DataBytesIn", "???\nThe number of data bytes received.\n"},
-  {"DataBytesOut", "???\nThe number of data bytes sent.\n"},
-  {"DataPktsIn", "???\nThe number of data packets received.\n"},
-  {"DataPktsOut", "???\nThe number of data packets sent.\n"},
+  {"DataBytesIn", "The number of octets contained in received data segments,\n"
+                  "including retransmitted data.  Note that this does not\n"
+                  "include TCP headers.\n"},
+  {"DataBytesOut", "The number of octets of data contained in transmitted segments,\n"
+                   "including retransmitted data.  Note that this does not include\n"
+                   "TCP headers.\n"},
+  {"DataPktsIn", "The number of segments received containing a positive length\n"
+                 "data segment.\n"},
+  {"DataPktsOut", "The number of segments sent containing a positive length data\n"
+                  "segment.\n"},
   {"DupAcksIn", "The number of duplicate ACKs received.\n"},
-  {"ECNEnabled", "???\nIf the explicit congestion notification is enabled.\n"},
+  {"ECNEnabled", "Enabled(1) if Explicit Congestion Notification (ECN)\n"
+                 "has been negotiated on,\n"
+                 "selfDisabled(2) if it is disabled or not implemented\n"
+                 "on the local host, or\n"
+                 "peerDisabled(3) if not negotiated by the remote hosts.\n"},
   {"FastRetran", "The number of invocations of the Fast Retransmit algorithm.\n"},
-  {"MaxCwnd", "???\nThe maximum congestion window, in octets.\n"},
+  {"MaxCwnd", "The maximum congestion window used during Slow Start, in octets.\n"},
   {"MaxMSS", "The maximum MSS, in octets.\n"},
   {"MaxRTO", "The maximum value of the retransmit timer RTO.\n"},
   {"MaxRTT", "The maximum sampled round trip time.\n"},
@@ -56,18 +66,19 @@ char* web100vartable[][2] = {
   {"MinRTT", "The minimum sampled round trip time.\n"},
   {"MinRwinRcvd", "???\nThe minimum window advertisement received, in octets.\n"},
   {"MinRwinSent", "???\nThe minimum window advertisement sent, in octets.\n"},
-  {"NagleEnabled", "???\nIf the Nagle algorithm is enabled.\n"},
+  {"NagleEnabled", "True(1) if the Nagle algorithm is being used, else false(2).\n"},
   {"OtherReductions", "The number of congestion window reductions made as a result\n"
                       "of anything other than AIMD congestion control algorithms.\n"
                       "Examples of non-multiplicative window reductions include\n"
                       "Congestion Window Validation [RFC2861] and experimental\n"
                       "algorithms such as Vegas.\n"},
-  {"PktsIn", "???\nThe number of packets received.\n"},
-  {"PktsOut", "???\nThe number of packets sent.\n"},
-  {"PktsRetrans", "???\nThe number of retransmitted packets.\n"},
-  {"RcvWinScale", "!!!\nThe value of the received window scale option if one was\n"
-                  "received; otherwise, a value of -1.\n"},
-  {"SACKEnabled", "???\nIf the SACK is enabled.\n"},
+  {"PktsIn", "The total number of segments received.\n"},
+  {"PktsOut", "The total number of segments sent.\n"},
+  {"PktsRetrans", "The number of segments transmitted containing at least some\n"
+                  "retransmitted data.\n"},
+  {"RcvWinScale", "The value of Rcv.Wind.Scale.  Note that\n"
+                  "RcvWinScale is either zero or the same as WinScaleSent.\n"},
+  {"SACKEnabled", "True(1) if SACK has been negotiated on, else false(2).\n"},
   {"SACKsRcvd", "The number of SACK options received.\n"},
   {"SendStall", "The number of interface stalls or other sender local\n"
                 "resource limitations that are treated as congestion\n"
@@ -78,11 +89,11 @@ char* web100vartable[][2] = {
                 "calculation of the RTO.\n"},
   {"SmoothedRTT", "The smoothed round trip time used in calculation of the\n"
                   "RTO. See SRTT in [RFC2988].\n"},
-  {"SndWinScale", "!!!\nThe value of the transmitted window scale option if one was\n"
-                  "sent; otherwise, a value of -1.\n"},
+  {"SndWinScale", "The value of Snd.Wind.Scale.  Note that\n"
+                  "SndWinScale is either zero or the same as WinScaleRcvd.\n"},
   {"SndLimTimeRwin", "The cumulative time spent in the 'Receiver Limited' state.\n"},
   {"SndLimTimeCwnd", "The cumulative time spent in the 'Congestion Limited' state.\n"},
-  {"SndLimTimeSender", "!!!\nThe cumulative time spent in the 'Sender Limited' state.\n"},
+  {"SndLimTimeSender", "The cumulative time spent in the 'Sender Limited' state.\n"},
   {"SndLimTransRwin", "The number of transitions into the 'Receiver Limited' state\n"
                       "from either the 'Congestion Limited' or 'Sender Limited'\n"
                       "states. This state is entered whenever TCP transmission\n"
@@ -95,7 +106,7 @@ char* web100vartable[][2] = {
                       "limit defined by congestion control (e.g. cwnd) or other\n"
                       "algorithms (retransmission timeouts) designed to control\n"
                       "network traffic.\n"},
-  {"SndLimTransSender", "!!!\nThe number of transitions into the 'Sender Limited' state\n"
+  {"SndLimTransSender", "The number of transitions into the 'Sender Limited' state\n"
                         "from either the 'Receiver Limited' or 'Congestion Limited'\n"
                         "states. This state is entered whenever TCP transmission\n"
                         "stops due to some sender limit such as running out of\n"
@@ -111,7 +122,7 @@ char* web100vartable[][2] = {
   {"SumRTT", "The sum of all sampled round trip times.\n"},
   {"Timeouts", "The number of times the retransmit timeout has expired when\n"
                "the RTO backoff multiplier is equal to one.\n"},
-  {"TimestampsEnabled", "!!!\nEnabled(1) if TCP timestamps have been negotiated on,\n"
+  {"TimestampsEnabled", "Enabled(1) if TCP timestamps have been negotiated on,\n"
                         "selfDisabled(2) if they are disabled or not implemented on\n"
                         "the local host, or peerDisabled(3) if not negotiated by the\n"
                         "remote hosts.\n"},
@@ -120,11 +131,12 @@ char* web100vartable[][2] = {
   {"WinScaleSent", "The value of the transmitted window scale option if one was\n"
                    "sent; otherwise, a value of -1.\n"},
   {"DupAcksOut", "The number of duplicate ACKs sent.\n"},
-  {"StartTimeUsec", "!!!\nThe value of sysUpTime at the time this listener was\n"
+  {"StartTimeUsec", "The value of sysUpTime at the time this listener was\n"
                     "established.  If the current state was entered prior to\n"
                     "the last re-initialization of the local network management\n"
                     "subsystem, then this object contains a zero value.\n"},
-  {"Duration", "???\nThe duration of the connection.\n"},
+  {"Duration", "The seconds part of the time elapsed between StartTimeStamp\n"
+               "and the most recent protocol event (segment sent or received).\n"},
   {NULL, NULL}
 };
 
