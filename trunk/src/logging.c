3efab0d51b1a28fa15f2845a9cdbbcf2c12a15ec
racarlson <racarlson@889c6698-20a3-11df-95ad-bbeafe1f0ab2>
2009-07-24 14:22:00 +0000

    Bug fixes
    web100-pcap.c: initial ifspeed value wasn't being set to -1
    
    converted from gethostbyaddr() to getnameinfo() routine.  getnameinfo() is v4/v6 compatible
    so I don't need to do the conversion.
    
    RAC 7/24/09

--- trunk/src/logging.c
+++ trunk/src/logging.c
@@ -12,6 +12,8 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <sys/time.h>
+#include <sys/socket.h>
+#include <netdb.h>
 
 #include "logging.h"
 
@@ -338,21 +340,27 @@ void writeMeta(void)
     char tmpstr[256], dir[128];
     char isoTime[64];
     size_t tmpstrlen=sizeof(tmpstr);
-    struct hostent *hp;
+    /* struct hostent *hp; */
+    socklen_t len;
 
 /* Get the clients domain name and same in metadata file 
+ * changed to use getnameinfo 7/24/09
  * RAC 7/7/09
  */
-#ifdef AF_INET6
-    if (meta.family == AF_INET6)
-	hp = (struct hostent *)gethostbyaddr((char *) &meta.client_ip, 16, AF_INET6);
-#endif
-    if (meta.family == AF_INET)
-	hp = (struct hostent *)gethostbyaddr((char *) &meta.client_ip, 4, AF_INET);
-    if (hp == NULL)
+/*
+ * #ifdef AF_INET6
+ *     if (meta.family == AF_INET6)
+ * 	hp = (struct hostent *)gethostbyaddr((char *) &meta.client_ip, 16, AF_INET6);
+ * #endif
+ *     if (meta.family == AF_INET)
+ * 	hp = (struct hostent *)gethostbyaddr((char *) &meta.client_ip, 4, AF_INET);
+ *     if (hp == NULL)
+ */
+    if (getnameinfo((struct sockaddr *)&meta.c_addr, len, tmpstr, tmpstrlen,
+	NULL, 0, NI_NAMEREQD))
 	memcpy(meta.client_name, "No FQDN name", 12);
     else
-	memcpy(meta.client_name, hp->h_name, strlen(hp->h_name));
+	memcpy(meta.client_name, tmpstr, tmpstrlen);
 
     memset(tmpstr, 0, tmpstrlen);
     strncpy(tmpstr, DataDirName, strlen(DataDirName));
