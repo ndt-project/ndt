56e05c648383f4b5f351ea0a23eea855d9c5258b
jwzurawski <jwzurawski@889c6698-20a3-11df-95ad-bbeafe1f0ab2>
2010-03-25 17:14:41 +0000

    bumbing version number to keep track of the test instances.
    -jason

--- branches/jz-localization/Applet/Tcpbw100.java
+++ branches/jz-localization/Applet/Tcpbw100.java
@@ -98,7 +98,7 @@ import java.security.PrivilegedAction;
 
 public class Tcpbw100 extends JApplet implements ActionListener
 {
-  private static final String VERSION = "3.6.2a";
+  private static final String VERSION = "3.6.2b";
   private static final byte TEST_MID = (1 << 0);
   private static final byte TEST_C2S = (1 << 1);
   private static final byte TEST_S2C = (1 << 2);
