c9e866c9eb5c7054e3c4ef5d8b011e7ad0c84fea
cmdln.net@gmail.com <cmdln.net@gmail.com@889c6698-20a3-11df-95ad-bbeafe1f0ab2>
2011-06-10 19:53:42 +0000

    Add new files to help define improved style, layout.

---
+++ branches/android/Android/src/net/measurementlab/ndt/AndroidNdt2.java
@@ -0,0 +1,23 @@
+// Copyright 2009 Google Inc. All Rights Reserved.
+
+package net.measurementlab.ndt;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+
+/**
+ * UI Thread and Entry Point of NDT mobile client.
+ */
+public class AndroidNdt2 extends Activity {
+
+	/**
+	 * Initializes the activity.
+	 */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.start);
+		Log.i("ndt", "Loaded!");
+	}
+}
