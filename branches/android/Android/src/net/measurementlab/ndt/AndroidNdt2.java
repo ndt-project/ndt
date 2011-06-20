27b7645df91f3431b9c5f34266ed97907def7571
cmdln.net@gmail.com <cmdln.net@gmail.com@889c6698-20a3-11df-95ad-bbeafe1f0ab2>
2011-06-20 16:23:24 +0000

    Add logging impl of ui services to better figure out how to fire activities in response to test progression.

--- branches/android/Android/src/net/measurementlab/ndt/AndroidNdt2.java
+++ branches/android/Android/src/net/measurementlab/ndt/AndroidNdt2.java
@@ -5,6 +5,8 @@ package net.measurementlab.ndt;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Typeface;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -16,7 +18,7 @@ import android.widget.TextView;
  * UI Thread and Entry Point of NDT mobile client.
  */
 public class AndroidNdt2 extends Activity {
-	
+
 	/**
 	 * Initializes the activity.
 	 */
@@ -25,17 +27,22 @@ public class AndroidNdt2 extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.start);
 		Log.i("ndt", "Loaded!");
-        Typeface typeFace = Typeface.createFromAsset(getAssets(),
-        "fonts/League_Gothic.otf");
-        TextView textView = (TextView) findViewById(R.id.MLabDesc);
-        textView.setTypeface(typeFace);
-        
-        Button startButton = (Button) findViewById(R.id.ButtonStart);
-        startButton.setOnClickListener(new OnClickListener() {
-			
+		Typeface typeFace = Typeface.createFromAsset(getAssets(),
+				"fonts/League_Gothic.otf");
+		TextView textView = (TextView) findViewById(R.id.MLabDesc);
+		textView.setTypeface(typeFace);
+
+		Button startButton = (Button) findViewById(R.id.ButtonStart);
+		startButton.setOnClickListener(new OnClickListener() {
+
 			@Override
 			public void onClick(View v) {
 				try {
+					Thread netWorker = new Thread(new NdtTests(
+							Constants.SERVER_HOST[Constants.DEFAULT_SERVER],
+							new Android2UiServices(), getNetworkType()));
+					netWorker.start();
+					
 					Intent intent = new Intent(Constants.SERVER_LOCATION);
 					startActivity(intent);
 				} catch (Throwable t) {
@@ -43,5 +50,21 @@ public class AndroidNdt2 extends Activity {
 				}
 			}
 		});
+	}
+
+	private String getNetworkType() {
+		ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
+		NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+		if (null == networkInfo) {
+			return NdtTests.NETWORK_UNKNOWN;
+		}
+		switch (networkInfo.getType()) {
+		case ConnectivityManager.TYPE_MOBILE:
+			return NdtTests.NETWORK_MOBILE;
+		case ConnectivityManager.TYPE_WIFI:
+			return NdtTests.NETWORK_WIFI;
+		default:
+			return NdtTests.NETWORK_UNKNOWN;
+		}
 	}
 }
