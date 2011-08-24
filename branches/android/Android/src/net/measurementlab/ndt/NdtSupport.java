30c156aadf54e9deedc61d65e6a6eb8c53006c5c
cmdln.net@gmail.com <cmdln.net@gmail.com@889c6698-20a3-11df-95ad-bbeafe1f0ab2>
2011-08-24 16:02:56 +0000

    Add mixin to clean repetitive code.

---
+++ branches/android/Android/src/net/measurementlab/ndt/NdtSupport.java
@@ -0,0 +1,58 @@
+package net.measurementlab.ndt;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import android.app.Activity;
+import android.graphics.Typeface;
+import android.widget.TextView;
+
+/**
+ * Mixin to share code useful across all parts of the app.
+ * 
+ * @author gideon@newamerica.net
+ * 
+ */
+class NdtSupport {
+	static final String DEFAULT_TYPEFACE = "fonts/League_Gothic.otf";
+
+	private static Map<String, Typeface> typefaces = new HashMap<String, Typeface>();
+
+	/**
+	 * Convenience that assumes a specific default font should be uniformaly
+	 * applied.
+	 * 
+	 * @see NdtSupport#applyFont(Activity, String, int...)
+	 */
+	static void applyFont(Activity activity, int... textFieldIds) {
+		applyFont(activity, DEFAULT_TYPEFACE, textFieldIds);
+	}
+
+	/**
+	 * Convenience for setting a whole array of text fields to the same
+	 * typeface.
+	 * 
+	 * @param activity
+	 *            used to find the fields to alter
+	 * @param typefaceResource
+	 *            the resource path to the typeface of interest
+	 * @param textFieldIds
+	 *            vararg of ids to look up and change
+	 */
+	static void applyFont(Activity activity, String typefaceResource,
+			int... textFieldIds) {
+		Typeface typeface = null;
+		if (typefaces.containsKey(typefaceResource)) {
+			typeface = typefaces.get(typefaceResource);
+		} else {
+			typeface = Typeface.createFromAsset(activity.getAssets(),
+
+			"fonts/League_Gothic.otf");
+			typefaces.put(typefaceResource, typeface);
+		}
+		for (int textFieldId : textFieldIds) {
+			TextView textView = (TextView) activity.findViewById(textFieldId);
+			textView.setTypeface(typeface);
+		}
+	}
+}
