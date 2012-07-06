18452101a7cc150a99e0453c61ac830ee9dc142e
Thomas Gideon <cmdln@thecommandline.net>
2012-07-06 14:28:45 +0000

    Merge and resolve, first past, cleaned up code from trunk.

--- src/mrange.c
+++ src/mrange.c
@@ -12,11 +12,12 @@
 
 #include "utils.h"
 #include "logging.h"
+#include "strlutils.h"
 
 typedef struct range {
-  int min;
-  int max;
-  struct range *next;
+	int min; /**< lower end of the range */
+	int max; /**< upper end of the range */
+	struct range *next; /**< pointer to the next range member */
 } Range;
 
 static Range* mrange_root;
@@ -30,91 +31,100 @@ static Range* mrange_root;
  *          1 - there was a syntax error in the string
  */
 
-int
-mrange_parse(char* text)
-{
-  char tmp[300];
-  char* ptr, *sptr;
-  Range* mr_ptr; 
-  
-  assert(text);
+int mrange_parse(char* text) {
+	char tmp[300];
+	char* ptr, *sptr;
+	Range* mr_ptr;
 
-  memset(tmp, 0, 300);
-  if (strlen(text) > 299) {
-    return 1;
-  }
-  strcpy(tmp, text);
-  ptr = strtok(tmp, ",");
-  while (ptr != NULL) {
-    if ((sptr = strchr(ptr, ':')) != NULL) {
-      *sptr++ = 0;
-      if (strchr(sptr, ':') != NULL) {
-        return 1;
-      }
-    }
-    else {
-      sptr = ptr;
-    }
-    if ((mr_ptr = malloc(sizeof(Range))) == NULL) {
-      log_println(0, "FATAL: cannot allocate memory");
-      return 1;
-    }
-    if (*ptr == 0) {
-      ptr = "1";
-    }
-    if (check_rint(ptr, &mr_ptr->min, 1, 65535)) {
-      free(mr_ptr);
-      return 1;
-    }
-    if (*sptr == 0) {
-      sptr = "65535";
-    }
-    if (check_rint(sptr, &mr_ptr->max, 1, 65535)) {
-      free(mr_ptr);
-      return 1;
-    }
-    mr_ptr->next = mrange_root;
-    mrange_root = mr_ptr;
-    ptr = strtok(NULL, ",");
-  }
-  free(mr_ptr);
-  return 0;
+	assert(text);
+
+	memset(tmp, 0, 300);
+	if (strlen(text) > 299) {
+		return 1;
+	}
+	//strcpy(tmp, text);
+	strlcpy(tmp, text, sizeof(tmp));
+	// tokenize based on a "," character.
+	// An example of the string : 2003:3000,4000:5000
+	ptr = strtok(tmp, ",");
+	while (ptr != NULL) { // tokens found
+		if ((sptr = strchr(ptr, ':')) != NULL) { // also found a ":" character,
+												 // with sptr pointing to its location
+			*sptr++ = 0;
+			if (strchr(sptr, ':') != NULL) { // should not find any more range
+				return 1;
+			}
+		} else {
+			sptr = ptr;
+		}
+		if ((mr_ptr = malloc(sizeof(Range))) == NULL) {
+			log_println(0, "FATAL: cannot allocate memory");
+			return 1;
+		}
+		if (*ptr == 0) {
+			ptr = "1";
+		}
+		// Check if the input string is within range and store
+		// result as "minimum"
+		// For ex: Is 2003 in a string like "2003:4000" within integer range ?
+		// If not, free the memory allocated to store the range and return
+		if (check_rint(ptr, &mr_ptr->min, 1, MAX_TCP_PORT)) {
+			free(mr_ptr);
+			return 1;
+		}
+		if (*sptr == 0) {
+			sptr = MAX_TCP_PORT_STR;
+		}
+		// now validate if "maximum" is within range  and store
+		// result as "maximum"
+		if (check_rint(sptr, &mr_ptr->max, 1, MAX_TCP_PORT)) {
+			free(mr_ptr);
+			return 1; // if invalid range, free allocated memory and return
+		}
+		mr_ptr->next = mrange_root;
+		mrange_root = mr_ptr; // ready to point to next member
+		ptr = strtok(NULL, ",");
+	}
+	free(mr_ptr);
+	return 0;
 }
 
-/*
- * Function name: mrange_parse
- * Description: Parses the string argument and adds the ranges to
- *              the free pool.
- * Arguments: text - the string containing ranges
- * Returns: 0 - the string was parsed correctly
- *          1 - there was a syntax error in the string
+/**
+ * Checks if a given number (passed in as string argument)
+ *  is available as a valid integer in the free pool. For example,
+ *  if we construct a list of valid port ranges, then this function
+ *  could be used to parse through the list to check for a usable port
+ *
+ * @param port  string containing port
+ * @returns char* containing 0 if the argument is invalid,
+ *  or the port passed as parameter, if valid
  */
 
 char*
-mrange_next(char* port)
-{
-  int val;
-  Range* ptr; 
+mrange_next(char* port) {
+	int val;
+	Range* ptr;
 
-  assert(port);
-  
-  if (check_rint(port, &val, 0, 65535)) {
-    log_println(0, "WARNING: invalid port number");
-    sprintf(port, "0");
-    return port;
-  }
-  val++;
-  while (val < 65536) {
-    ptr = mrange_root;
-    while (ptr != NULL) {
-      if ((val >= ptr->min) && (val <= ptr->max)) {
-        sprintf(port, "%d", val);
-        return port;
-      }
-      ptr = ptr->next;
-    }
-    val++;
-  }
-  sprintf(port, "0");
-  return port;
+	assert(port);
+
+	if (check_rint(port, &val, 0, MAX_TCP_PORT)) { // check if valid
+		log_println(0, "WARNING: invalid port number");
+		sprintf(port, RESERVED_PORT);
+		return port;
+	}
+	val++;
+	while (val <= MAX_TCP_PORT) { // Maximum port number not exceeded
+		ptr = mrange_root;
+		while (ptr != NULL) { // While there is some data
+			if ((val >= ptr->min) && (val <= ptr->max)) { // check range
+														  // and return port if valid
+				sprintf(port, "%d", val);
+				return port;
+			}
+			ptr = ptr->next;
+		}
+		val++;
+	}
+	sprintf(port, RESERVED_PORT);
+	return port;
 }
--- src/testoptions.h
+++ src/testoptions.h
@@ -11,47 +11,73 @@
 
 #include "web100srv.h"
 
+
+#define LISTENER_SOCKET_CREATE_FAILED  -1
+#define SOCKET_CONNECT_TIMEOUT  -100
+#define RETRY_EXCEEDED_WAITING_CONNECT -101
+#define RETRY_EXCEEDED_WAITING_DATA -102
+#define SOCKET_STATUS_FAILED -1
+
+
 typedef struct testoptions {
-  int multiple;
-  int mainport;
-  
-  int midopt;
-  int midsockfd;
-  int midsockport;
-  
-  int c2sopt;
-  int c2ssockfd;
-  int c2ssockport;
-  
-  int s2copt;
-  int s2csockfd;
-  int s2csockport;
+	int multiple; // multiples tests enabled
+	int mainport; // main port used for test
 
-  pid_t child0;
-  pid_t child1;
-  pid_t child2;
+	int midopt;	  // middlebox test to be perfomed?
+	int midsockfd; // socket file desc for middlebox test
+	int midsockport; // port used for middlebox test
 
-  int sfwopt;
-  int State;
+	int c2sopt;     // C2S test to be perfomed?
+	int c2ssockfd;  // socket fd for C2S test
+	int c2ssockport; // port used for C2S test
 
-  int metaopt;
+	int s2copt;   // S2C test to be perfomed?
+	int s2csockfd; // socket fd for S2C test
+	int s2csockport; // port used for S2C test
+
+	// child pids
+	pid_t child0;
+	pid_t child1;
+	pid_t child2;
+
+	int sfwopt;	// Is firewall test to be performed?
+	int State; // seems unused currently
+
+	int metaopt; // meta test to be perfomed?
 } TestOptions;
+
+// Snap log characteristics
+typedef struct snapArgs {
+	web100_snapshot* snap; // web_100 snapshot indicator
+	web100_log* log; // web_100 log
+	int delay; // periodicity, in ms, of collecting snap
+} SnapArgs;
 
 int wait_sig;
 
-int initialize_tests(int ctlsockfd, TestOptions* testOptions, char * test_suite);
+int initialize_tests(int ctlsockfd, TestOptions* testOptions,
+		char * test_suite);
 
-int test_mid(int ctlsockfd, web100_agent* agent, TestOptions* testOptions, int conn_options, double* s2c2spd);
-int test_c2s(int ctlsockfd, web100_agent* agent, TestOptions* testOptions, int conn_options, double* c2sspd,
-    int set_buff, int window, int autotune, char* device, Options* options,
-    int record_reverse, int count_vars, char spds[4][256], int* spd_index);
-int test_s2c(int ctlsockfd, web100_agent* agent, TestOptions* testOptions, int conn_options, double* s2cspd,
-    int set_buff, int window, int autotune, char* device, Options* options, char spds[4][256],
-    int* spd_index, int count_vars, CwndPeaks* peaks);
-int test_sfw_srv(int ctlsockfd, web100_agent* agent, TestOptions* options, int conn_options);
-int test_meta_srv(int ctlsockfd, web100_agent* agent, TestOptions* options, int conn_options);
+void catch_s2c_alrm(int signo);
+
+int test_sfw_srv(int ctlsockfd, web100_agent* agent, TestOptions* options,
+		int conn_options);
+int test_meta_srv(int ctlsockfd, web100_agent* agent, TestOptions* options,
+		int conn_options);
 
 int getCurrentTest();
 void setCurrentTest(int testId);
 
+void start_snap_worker(SnapArgs *snaparg, web100_agent *agentarg,
+                char snaplogenabled,  pthread_t *wrkrthreadidarg,
+                char *metafilevariablename, char *metafilename, web100_connection* conn,
+                web100_group* group);
+
+void stop_snap_worker(int *workerThreadId, char snaplogenabled,
+                SnapArgs* snapArgs_ptr);
+
+void setCwndlimit(web100_connection* connarg, web100_group* grouparg,
+		web100_agent* agentarg, Options* optionsarg);
+
+int is_buffer_clogged(int nextseqtosend, int lastunackedseq);
 #endif
