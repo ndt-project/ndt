1cf29652205cdb33baf2946ea8e23e35c8072610
skostuch@soldevelo.com <skostuch@soldevelo.com@889c6698-20a3-11df-95ad-bbeafe1f0ab2>
2014-06-20 09:08:57 +0000

    Tagged NDT version 3.7.0-rc1

---
+++ tags/release_candidate_3.7.0/src/test_sfw.h
@@ -0,0 +1,26 @@
+/*
+ * This file contains the function declarations to handle simple
+ * firewall test.
+ *
+ * Jakub S�awi�ski 2006-07-15
+ * jeremian@poczta.fm
+ */
+
+#ifndef SRC_TEST_SFW_H_
+#define SRC_TEST_SFW_H_
+
+#define SFW_NOTTESTED  0
+#define SFW_NOFIREWALL 1
+#define SFW_UNKNOWN    2
+#define SFW_POSSIBLE   3
+
+#define SFW_TEST_DEFAULT_LEN 20
+#define SFW_PREDEFINED_TEST_MSG "Simple firewall test"
+#define SFW_TEST_LOG "Simple firewall test"
+#define EMPHERAL_PORT_NUMBER "empheralPortNumber"
+#define TEST_TIME "testTime"
+
+int test_sfw_clt(int ctlsockfd, char tests, char* host, int conn_options, int jsonSupport);
+int results_sfw(char tests, char* host);
+
+#endif  // SRC_TEST_SFW_H_
