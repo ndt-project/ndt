e4322039c3bd2e21de832b7682731f85583fce2e
skostuch@soldevelo.com <skostuch@soldevelo.com@889c6698-20a3-11df-95ad-bbeafe1f0ab2>
2015-03-31 14:01:01 +0000

    Remove unused applet_54 branch

--- branches/applet_54/janalyze/src/MaxPeakInfo.java
+++
@@ -1,9 +0,0 @@
-class MaxPeakInfo extends PeakInfo {
-    MaxPeakInfo(int cwnd, int sampleRTT) {
-        super(cwnd, sampleRTT);
-    }
-
-    public String toString() {
-        return "max: " + super.toString();
-    }
-}
