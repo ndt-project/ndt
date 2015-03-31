fb6946de25b7d6fa77de7985134e96f32c1c4c6b
skostuch@soldevelo.com <skostuch@soldevelo.com@889c6698-20a3-11df-95ad-bbeafe1f0ab2>
2015-03-31 13:47:14 +0000

    Remove unused FlashIssue108 branch

--- branches/FlashIssue108/janalyze/src/MinPeakInfo.java
+++
@@ -1,9 +0,0 @@
-class MinPeakInfo extends PeakInfo {
-    MinPeakInfo(int cwnd, int sampleRTT) {
-        super(cwnd, sampleRTT);
-    }
-
-    public String toString() {
-        return "min: " + super.toString();
-    }
-}
