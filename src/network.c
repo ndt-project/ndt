6c8013422a50598652de9952d90efb1161d3a45f
Peter Boothe <pboothe@google.com>
2015-11-19 16:59:55 +0000

    connection_close now calls SSL_shutdown

--- src/network.c
+++ src/network.c
@@ -717,6 +717,9 @@ void shutdown_connection(Connection *conn) {
  * @param conn The connection to close
  */
 void close_connection(Connection *conn) {
+  if (conn->ssl != NULL) {
+    SSL_shutdown(conn->ssl);
+  }
   SSL_free(conn->ssl);
   conn->ssl = NULL;
   close(conn->socket);
